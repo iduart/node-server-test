9c8d1aba808fd0acc3dacd79b73016df039e04f2
Iduart <iduartdean@gmail.com>
2021-04-17 16:11:42 -0500

    asdas

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+console.log("PORT".repeat(200), process.env.PORT);
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
